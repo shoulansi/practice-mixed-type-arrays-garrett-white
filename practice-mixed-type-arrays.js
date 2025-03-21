ed08b1df48afb65de27cecef4fd5bce74f7a4cfd
shoulansi <garrettswhite@gmail.com>
2025-03-21 11:19:51 -0500

    final mixed array

--- practice-mixed-type-arrays.js
+++ practice-mixed-type-arrays.js
@@ -1,1 +1,13 @@
+let userProfile = [
+    "Garrett",
+    22,
+    false,
+    {city: "St.Louis"},
+    ["Video Games","Rock Climbing"]
+];
+console.log(userProfile[0], userProfile[4][1]);
 
+userProfile[1] = 23;
+userProfile[4][userProfile[4].length] = "Cooking"
+
+console.log(userProfile);
